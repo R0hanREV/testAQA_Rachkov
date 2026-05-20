98fbc468a4146d3eee2bcacf5ef29ad4d39c3acd
E_Rachkov <Totenkopf1986@gmail.com>
2026-05-20 13:04:05 +0300

    lesson_4

--- src/main/java/lesson_4/Main.java
+++ src/main/java/lesson_4/Main.java
@@ -3,9 +3,12 @@ package lesson_4;
 public class Main {
     public static void main(String[] args) {
  //       Dog dog1 = new Dog("Bobik");
-        Cat cat1 = new Cat("Murzik");
-  //     cat1.run(150);
-  //     cat1.swim(10);
+        Cat [] cat1 = new Cat[3];
+        cat1[0] = new Cat("Murzik");
+        cat1[1]= new Cat("Barsik");
+        cat1[2] = new Cat("Begemot");
+        //     cat1[0].run(150);
+  //     cat1[0].swim(10);
     Bowl bowl1 = new Bowl();
     bowl1.setAmountOfFood(50);
     cat1.eat(bowl1, 10);
