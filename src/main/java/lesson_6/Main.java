ec52b31b47339a31fb81eb00f645963ad0ccf055
E_Rachkov <Totenkopf1986@gmail.com>
2026-05-27 11:41:02 +0300

    lesson_6

--- src/main/java/lesson_6/Main.java
+++ src/main/java/lesson_6/Main.java
@@ -4,31 +4,33 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-      //  HashSet<Student> students = new HashSet<>();
-      //  students.add(new Student("Иван Иванов", "А2", 2, Arrays.asList(4, 2, 4, 5, 3)));
-      //  students.add(new Student("Петр Петров", "А2", 2, Arrays.asList(4, 2, 4, 5, 3)));
-      //  students.add(new Student("Иван Сидоров", "B3", 3, Arrays.asList(3, 2, 1, 2, 3)));
-      //  students.add(new Student("Олег Кузнецов", "B1", 1, Arrays.asList(3, 3, 2, 5, 1)));
-      //  students.add(new Student("Игорь Яковлев", "B4", 4, Arrays.asList(4, 4, 4, 3, 3)));
-//
-      //  for (Student o : students) {
-      //      System.out.println(o);
-      //  }
-      //  StudentManage.printStudents(students, 2);
-      //  StudentManage.removeStudent(students);
-      //  StudentManage.promoteStudents(students);
-      //  for (Student o : students) {
-      //      System.out.println(o);
-      //  }
+        HashSet<Student> students = new HashSet<>();
+        students.add(new Student("Иван Иванов", "А2", 2, Arrays.asList(4, 2, 4, 5, 3)));
+        students.add(new Student("Петр Петров", "А2", 2, Arrays.asList(4, 2, 4, 5, 3)));
+        students.add(new Student("Иван Сидоров", "B3", 3, Arrays.asList(3, 2, 1, 2, 3)));
+        students.add(new Student("Олег Кузнецов", "B1", 1, Arrays.asList(3, 3, 2, 5, 1)));
+        students.add(new Student("Игорь Яковлев", "B4", 4, Arrays.asList(4, 4, 4, 3, 3)));
+
+        for (Student o : students) {
+            System.out.println(o);
+        }
+        StudentManage.printStudents(students, 2);
+        StudentManage.removeStudent(students);
+        StudentManage.promoteStudents(students);
+        for (Student o : students) {
+            System.out.println(o);
+        }
+
+
         PhoneBook phoneBook = new PhoneBook();
         phoneBook.add("Иванов", "8-901-111-11-11");
-    //  phoneBook.add("Иванов", "8-902-222-22-22");
-    //  phoneBook.add("Иванов", "8-903-333-33-33");
-    //  phoneBook.add("Петров", "8-904-444-44-44");
-    //  phoneBook.add("Петров", "8-905-555-55-55");
-    //  phoneBook.add("Сидоров", "8-906-666-66-66");
+        phoneBook.add("Иванов", "8-901-111-11-11");
+        phoneBook.add("Иванов", "8-902-222-22-22");
+        phoneBook.add("Иванов", "8-903-333-33-33");
+        phoneBook.add("Петров", "8-904-444-44-44");
+        phoneBook.add("Петров", "8-905-555-55-55");
+        phoneBook.add("Сидоров", "8-906-666-66-66");
 
-        phoneBook.get("Петров");
-        phoneBook.printAll();
+        phoneBook.get("Иванов");
     }
 }
--- src/main/java/lesson_6/PhoneBook.java
+++ src/main/java/lesson_6/PhoneBook.java
@@ -1,5 +1,6 @@
 package lesson_6;
 
+import javax.xml.namespace.QName;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,26 +19,11 @@ public class PhoneBook {
                 System.out.println(name + " : " + o.getValue());
             }
         }
-
     }
 
     public void add(String name, String phoneNumber) {
-        for (Map.Entry<String, List<String>> o : phoneBook.entrySet()) {
-            if (!phoneBook.containsKey(name)) {
-                phoneBook.put(name, new ArrayList<>());
-            }
-            if (o.getKey().equals(name)) {
-                o.getValue().add(phoneNumber);
-            }
-        }
-    }
-
-    void printAll() {
-        System.out.println("\n=== ВЕСЬ ТЕЛЕФОННЫЙ СПРАВОЧНИК ===");
-        for (Map.Entry<String, List<String>> entry : phoneBook.entrySet()) {
-            System.out.print(entry.getKey() + ": ");
-            System.out.println(String.join(", ", entry.getValue()));
-
-        }
+        List<String> phones = phoneBook.computeIfAbsent(name, k -> new ArrayList<>());
+        phones.add(phoneNumber);
     }
 }
+
