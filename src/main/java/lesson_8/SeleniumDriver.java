e701a465c928c31a0d14496db63a9027de229895
E_Rachkov <Totenkopf1986@gmail.com>
2026-05-31 14:56:59 +0300

    lesson_8

--- src/main/java/lesson_8/SeleniumDriver.java
+++ src/main/java/lesson_8/SeleniumDriver.java
@@ -4,16 +4,22 @@ import lombok.Setter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.util.Objects;
 
 public class SeleniumDriver {
 
     private final static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
+    public static void setOptions(ChromeOptions options) {
+        SeleniumDriver.options = options;
+    }
+
+    private static ChromeOptions options;
 
 
-    public static WebDriver getInstance(){
 
+    public static WebDriver getInstance() {
         return driver.get();
     }
 }
