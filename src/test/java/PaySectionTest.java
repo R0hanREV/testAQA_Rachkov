58fd5470ccdc4ce69033a9b41bde40a126395712
E_Rachkov <Totenkopf1986@gmail.com>
2026-06-02 15:26:16 +0300

    lesson_8

--- src/test/java/PaySectionTest.java
+++ src/test/java/PaySectionTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -15,7 +17,7 @@ public class PaySectionTest extends BaseTest {
     @BeforeEach
     public void homePage() {
         driver.get("https://www.mts.by/");
-
+      //  driver.findElement(By.id("cookie-agree")).click();
     }
 
     public void coockie() {
@@ -44,61 +46,67 @@ public class PaySectionTest extends BaseTest {
     @ParameterizedTest
     @ValueSource(strings = {"Visa", "Verified By Visa", "MasterCard", "MasterCard Secure Code", "Белкарт"})
     public void checkLogoPartners(String partner) {
-        List<WebElement> logo = driver.findElements(By.xpath("//section[@class='pay']//div[@class='pay__partners']//img"));
+        List<WebElement> logo = driver.findElements(By.xpath("//section[@class='pay']//div[@class='pay__partners']//img[@alt='" + partner + "']"));
         for (WebElement o : logo) {
             String actualLogo = o.getAttribute("alt");
-            Assertions.assertEquals(actualLogo, partner);
+            Assertions.assertEquals(partner, actualLogo);
         }
     }
 
 
-    @Test
+    @ParameterizedTest
     @DisplayName("Проверка логотипа Visa")
-    public void checkLogoVisa() {
-        WebElement logo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//li[1]/img"));
-        Assertions.assertEquals(logo.getAttribute("alt"), "Visa");
+    @ValueSource(strings = {"Visa"})
+    public void checkLogoVisa(String partner) {
+        WebElement logo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//img[@alt='" + partner + "']"));
+        Assertions.assertEquals(partner, logo.getAttribute("alt"));
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("Проверка логотипа Verified By Visa")
-    public void checkLogoVisaVerify() {
-        WebElement logo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//li[2]/img"));
-        Assertions.assertEquals(logo.getAttribute("alt"), "Verified By Visa");
+    @ValueSource(strings = {"Verified By Visa"})
+    public void checkLogoVisaVerify(String partner) {
+        WebElement logo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//img[@alt='" + partner + "']"));
+        Assertions.assertEquals(partner, logo.getAttribute("alt"));
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("Проверка логотипа MasterCard")
-    public void checkLogoMasterCard() {
-        WebElement logo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//li[3]/img"));
-        Assertions.assertEquals(logo.getAttribute("alt"), "MasterCard");
+    @ValueSource(strings = {"MasterCard"})
+    public void checkLogoMasterCard(String partner) {
+        WebElement logo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//img[@alt='" + partner + "']"));
+        Assertions.assertEquals(partner, logo.getAttribute("alt"));
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("Проверка логотипа MasterCard Secure Code")
-    public void checkLogoMasterCardSecure() {
-        WebElement logo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//li[4]/img"));
-        Assertions.assertEquals(logo.getAttribute("alt"), "MasterCard Secure Code");
+    @ValueSource(strings = {"MasterCard Secure Code"})
+    public void checkLogoMasterCardSecure(String partner) {
+        WebElement logo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//img[@alt='" + partner + "']"));
+        Assertions.assertEquals(partner, logo.getAttribute("alt"));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Проверка логотипа Белкарт")
+    @ValueSource(strings = {"Белкарт"})
+    public void checkLogoBelcard(String partner) {
+        WebElement logo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//img[@alt='" + partner + "']"));
+        Assertions.assertEquals(partner, logo.getAttribute("alt"));
     }
 
     @Test
-    @DisplayName("Проверка логотипа Белкарт")
-    public void checkLogoBelcard() {
-        WebElement logo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//li[5]/img"));
-        Assertions.assertEquals(logo.getAttribute("alt"), "Белкарт");
-    }
-
-    public void checkButtonContinue() {
+    public void checkButtonContinue() throws InterruptedException {
         WebElement phone = driver.findElement(By.id("connection-phone"));
         WebElement payment = driver.findElement(By.id("connection-sum"));
         WebElement mail = driver.findElement(By.id("connection-email"));
         WebElement buttonContinue = driver.findElement(By.xpath("//*[@id='pay-connection']/button"));
-        WebElement modelWindow = driver.findElement();
 
         phone.sendKeys("297777777");
         payment.sendKeys("100");
         mail.sendKeys("qwerty@mail.ru");
         buttonContinue.click();
-        Thread.sleep(5000);
-
+        Thread.sleep(10000);
+        WebElement modelWindow = driver.findElement(By.xpath("//iframe[@class='payment-widget-iframe']"));
+        Assertions.assertTrue(modelWindow.isDisplayed());
     }
 }
